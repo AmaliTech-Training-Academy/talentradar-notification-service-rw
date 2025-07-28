40a0b59afde6515742b8e3b787603f53324ef310
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-07-28 14:23:26 +0200

    fix: added the fix for web sockets

--- src/main/java/com/talentradar/talentradarnotificationservicerw/config/WebSocketConfig.java
+++ src/main/java/com/talentradar/talentradarnotificationservicerw/config/WebSocketConfig.java
@@ -1,19 +1,10 @@
 package com.talentradar.talentradarnotificationservicerw.config;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.config.ChannelRegistration;
+
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -42,21 +33,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setUserDestinationPrefix("/user");
     }
 
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new ChannelInterceptor() {
-            @Override
-            public Message<?> preSend(Message<?> message, MessageChannel channel) {
-                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-                if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
-                    Authentication user = (Authentication) accessor.getSessionAttributes().get("user");
-                    if (user != null) {
-                        accessor.setUser(user);
-                        SecurityContextHolder.getContext().setAuthentication(user);
-                    }
-                }
-                return message;
-            }
-        });
-    }
 }
